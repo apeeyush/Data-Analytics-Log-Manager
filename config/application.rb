ec918d01b8c00c28f56e8a141c175720c5724872
peeyush <peeyushagarwal1994@gmail.com>
2014-05-25 11:57:04 +0530

    Set 'Access-Control-Allow-Origin' header to allow requests from other origins

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,12 @@ module LogManager
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Set Access-Control-Allow-Origin' header to allow ajax requests from other origins
+    config.action_dispatch.default_headers = {
+      'Access-Control-Allow-Origin' => '*',
+      'Access-Control-Request-Method' => '*'
+    }
+
   end
 end
