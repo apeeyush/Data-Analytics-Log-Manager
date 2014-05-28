5f1ef147c146b680d159232dac9ef75697df8224
peeyush <peeyushagarwal1994@gmail.com>
2014-05-28 15:46:12 +0530

    Send multiple logs at once to the API

--- app/controllers/api/logs_controller.rb
+++ app/controllers/api/logs_controller.rb
@@ -33,26 +33,57 @@ module Api
     end
 
   	def create
-  	  log = Log.new()
-  	  log[:session] = params[:session]
-  	  log[:user] = params[:user]
-  	  log[:application] = params[:application]
-  	  log[:activity] = params[:activity]
-  	  log[:event] = params[:event]
-  	  log[:time] = DateTime.strptime("#{params[:time].to_i/1000}",'%s').in_time_zone("Eastern Time (US & Canada)").to_s
-  	  log[:parameters] = params[:parameters]
-  	  log[:extras] = Hash.new
-      request.request_parameters.each do |key,value|
-        if key != "log" && key != "session" && key != "user" && key != "application" && key != "activity" && key != "event" && key != "time" && key != "parameters"
-          log[:extras][key] = value
+      if (!params.has_key?(:logs))
+    	  log = Log.new()
+    	  log[:session] = params[:session]
+    	  log[:user] = params[:user]
+    	  log[:application] = params[:application]
+    	  log[:activity] = params[:activity]
+    	  log[:event] = params[:event]
+    	  log[:time] = DateTime.strptime("#{params[:time].to_i/1000}",'%s').in_time_zone("Eastern Time (US & Canada)").to_s
+    	  log[:parameters] = params[:parameters]
+    	  log[:extras] = Hash.new
+        request.request_parameters.each do |key,value|
+          if key != "log" && key != "session" && key != "user" && key != "application" && key != "activity" && key != "event" && key != "time" && key != "parameters"
+            log[:extras][key] = value
+          end
         end
+    	  if log.save
+    	  	render json: log, status: :created
+    	  else
+    	  	render json: log.errors, status: 422
+    	  end
+      
+      elsif (params.has_key?(:logs) && params[:logs] == 'multiple')
+        logger.debug("Request Parameters\n")
+        logger.debug(request.request_parameters)
+        logger.debug("\nIndividual values\n")
+        request.request_parameters.each do |key, values|
+          if key == "_json"
+            values.each do |value|
+              logger.debug(value)
+              log = Log.new()
+              log[:session] = value[:session]
+              log[:user] = value[:user]
+              log[:application] = value[:application]
+              log[:activity] = value[:activity]
+              log[:event] = value[:event]
+              log[:time] = DateTime.strptime("#{value[:time].to_i/1000}",'%s').in_time_zone("Eastern Time (US & Canada)").to_s
+              log[:parameters] = value[:parameters]
+              log[:extras] = Hash.new
+              value.each do |key, value|
+                if key != "log" && key != "session" && key != "user" && key != "application" && key != "activity" && key != "event" && key != "time" && key != "parameters"
+                  log[:extras][key] = value
+                end
+              end
+              if !log.save
+                render json: log.errors, status: 422
+              end
+            end
+          end
+        end
+        render text: '', status: :created
       end
-
-  	  if log.save
-  	  	render json: log, status: :created
-  	  else
-  	  	render json: log.errors, status: 422
-  	  end
   	end
 
   end
