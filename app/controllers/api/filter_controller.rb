223be5c6048368e3bc1d5fe0e8878e9e994dd657
peeyush <peeyushagarwal1994@gmail.com>
2014-05-31 19:02:21 +0530

    Filter Log Data directly from Data Interactive
    Go to http://../codap and select Data Interactive after logging in. Enter the JSON filter body in the textarea and click the button below.

--- app/controllers/api/filter_controller.rb
+++ app/controllers/api/filter_controller.rb
@@ -7,48 +7,49 @@ module Api
 
     def index
       request_body = JSON.parse(request.body.read)
-      filter = request_body["filter"]
       logs = Log.all
-      if (filter["application"] != nil && !filter["application"].empty?)
-      	logs = logs.where({application: filter["application"]})
+      if (request_body["filter"] != nil)
+        filter = request_body["filter"]
+        if (filter["application"] != nil && !filter["application"].empty?)
+        	logs = logs.where({application: filter["application"]})
+        end
+        if (filter["activity"] != nil && !filter["activity"].empty?)
+        	logs = logs.where({activity: filter["activity"]})
+        end
+        if (filter["user"] != nil && !filter["user"].empty?)
+        	logs = logs.where({user: filter["user"]})
+        end
+        if (filter["event"] != nil && !filter["event"].empty?)
+        	logs = logs.where({event: filter["event"]})
+        end
+        if (filter["time"] != nil && !filter["time"].empty?)
+        	logs = logs.where("time >= :start_time AND time <= :end_time",{start_time: filter["time"][0], end_time: filter["time"][1]})
+        end
+        if (filter["parameters"] != nil && filter["parameters"]["keys"] != nil && !filter["parameters"]["keys"].empty?)
+        	keys = filter["parameters"]["keys"]
+        	keys.each do |key|
+        	  logs = logs.where("parameters ? :key", key: key)
+        	end
+        end
+        if (filter["parameters"] != nil && filter["parameters"]["pairs"] != nil && !filter["parameters"]["pairs"].empty?)
+        	pairs = filter["parameters"]["pairs"]
+        	pairs.each do |key, value|
+        	  logs = logs.where("parameters @> (:key => :value)", key: key, value: value)
+        	end
+        end
+        if (filter["extras"] != nil && filter["extras"]["keys"] != nil && !filter["extras"]["keys"].empty?)
+        	keys = filter["extras"]["keys"]
+        	keys.each do |key|
+        	  logs = logs.where("extras ? :key", key: key)
+        	end
+        end
+        if (filter["extras"] != nil && filter["extras"]["pairs"] != nil && !filter["extras"]["pairs"].empty?)
+        	pairs = filter["extras"]["pairs"]
+        	pairs.each do | key, value|
+        	  logs = logs.where("extras @> (:key => :value)", key: key, value: value)
+        	end
+        end
       end
-      if (filter["activity"] != nil && !filter["activity"].empty?)
-      	logs = logs.where({activity: filter["activity"]})
-      end
-      if (filter["user"] != nil && !filter["user"].empty?)
-      	logs = logs.where({user: filter["user"]})
-      end
-      if (filter["event"] != nil && !filter["event"].empty?)
-      	logs = logs.where({event: filter["event"]})
-      end
-      if (filter["time"] != nil && !filter["time"].empty?)
-      	logs = logs.where("time >= :start_time AND time <= :end_time",{start_time: filter["time"][0], end_time: filter["time"][1]})
-      end
-      if (filter["parameters"] != nil && filter["parameters"]["keys"] != nil && !filter["parameters"]["keys"].empty?)
-      	keys = filter["parameters"]["keys"]
-      	keys.each do |key|
-      	  logs = logs.where("parameters ? :key", key: key)
-      	end
-      end
-      if (filter["parameters"] != nil && filter["parameters"]["pairs"] != nil && !filter["parameters"]["pairs"].empty?)
-      	pairs = filter["parameters"]["pairs"]
-      	pairs.each do |key, value|
-      	  logs = logs.where("parameters @> (:key => :value)", key: key, value: value)
-      	end
-      end
-      if (filter["extras"] != nil && filter["extras"]["keys"] != nil && !filter["extras"]["keys"].empty?)
-      	keys = filter["extras"]["keys"]
-      	keys.each do |key|
-      	  logs = logs.where("extras ? :key", key: key)
-      	end
-      end
-      if (filter["extras"] != nil && filter["extras"]["pairs"] != nil && !filter["extras"]["pairs"].empty?)
-      	pairs = filter["extras"]["pairs"]
-      	pairs.each do | key, value|
-      	  logs = logs.where("extras @> (:key => :value)", key: key, value: value)
-      	end
-      end
-
 	  if logs != nil
       	@logs = logs
       else
