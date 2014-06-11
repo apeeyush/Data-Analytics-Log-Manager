8c8175e5a3b351e74e25a9db25e3cbf025e0e6e1
peeyush <peeyushagarwal1994@gmail.com>
2014-06-11 12:34:56 +0530

    Use others for specifying additional filters.
    No need to use separate 'parameters' and 'extras'.

--- app/models/log.rb
+++ app/models/log.rb
@@ -52,7 +52,7 @@ class Log < ActiveRecord::Base
     end
     string_columns = []
     time_columns = []
-    hstore_columns = []
+    hstore_columns = ""
     logs_columns.except!("id")
     logs_columns.each do |column_name, type|
       if type == :string
@@ -60,9 +60,10 @@ class Log < ActiveRecord::Base
       elsif type == :datetime
         time_columns << column_name
       elsif type == :hstore
-        hstore_columns << column_name
+        hstore_columns == "" ? hstore_columns << column_name : hstore_columns << " || " + column_name
       end
     end
+    logger.debug(hstore_columns)
     string_columns.each do |string_column|
       if (filter[string_column] != nil && !filter[string_column].empty? )
         if filter[string_column]["type"] == "remove"
@@ -77,33 +78,25 @@ class Log < ActiveRecord::Base
        logs = logs.where("#{time_column} >= :start_time AND time <= :end_time",{start_time: filter["time"][0], end_time: filter["time"][1]})
       end
     end
-    hstore_columns.each do |hstore_column|
-      if (filter[hstore_column] != nil && filter[hstore_column]["keys"] != nil && !filter[hstore_column]["keys"].empty?)
-        keys = filter[hstore_column]["keys"]
-        if !keys["list"].empty?
-          if keys["type"] == "remove"
-            keys["list"].each do |key|
-              logs = logs.where("NOT #{hstore_column} ? :key", key: key)
-            end
-          else
-            keys["list"].each do |key|
-              logs = logs.where("#{hstore_column} ? :key", key: key)
-            end
-          end
-        end
+    if (filter["others"] != nil && filter["others"]["keys"] != nil && !filter["others"]["keys"].empty? && !filter["others"]["keys"]["list"].empty?)
+      keys = filter["others"]["keys"]
+      if keys["type"] == "remove"
+        logs = logs.where("NOT #{hstore_columns} ?& ARRAY[:keys]", keys: keys["list"])
+      else
+        logs = logs.where("#{hstore_columns} ?& ARRAY[:keys]", keys: keys["list"])
       end
-      if (filter[hstore_column] != nil && filter[hstore_column]["pairs"] != nil && !filter[hstore_column]["pairs"].empty?)
-        pairs = filter[hstore_column]["pairs"]
-        if pairs["type"] == "remove"
-          pairs["list"].each do |pair|
-            key = pair.keys[0]
-            logs = logs.where.not("#{hstore_column} @> hstore(:key,:value)", :key => pair.keys[0], :value => pair[key])
-          end
-        else
-          pairs["list"].each do |pair|
-            key = pair.keys[0]
-            logs = logs.where("#{hstore_column} @> hstore(:key,:value)", :key => pair.keys[0], :value => pair[key])
-          end
+    end
+    if (filter["others"] != nil && filter["others"]["pairs"] != nil && !filter["others"]["pairs"].empty?)
+      pairs = filter["others"]["pairs"]
+      if pairs["type"] == "remove"
+        pairs["list"].each do |pair|
+          key = pair.keys[0]
+          logs = logs.where.not("#{hstore_columns} @> hstore(:key,:value)", :key => pair.keys[0], :value => pair[key])
+        end
+      else
+        pairs["list"].each do |pair|
+          key = pair.keys[0]
+          logs = logs.where("#{hstore_columns} @> hstore(:key,:value)", :key => pair.keys[0], :value => pair[key])
         end
       end
     end
