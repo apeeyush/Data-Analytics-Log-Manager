c826a24f2ef3e3fd50ce5c3fb14a7fb604429a2e
peeyush <peeyushagarwal1994@gmail.com>
2014-06-26 20:12:49 +0530

    Bug fix
    Previously, Count added nothing when the count was 0. This used to mis-align the parent table.

--- app/controllers/api/group_transform_controller.rb
+++ app/controllers/api/group_transform_controller.rb
@@ -18,10 +18,12 @@ module Api
       if group != nil && !group.empty? && (group =="username" || group == "activity" || group == "application" || group == "session")
         @groups = Hash.new
         parent = group
+        parents_list = []
         logs.select(parent).group(parent).order(parent).each do |log|
           @groups[log[parent]] = Hash.new
           @groups[log[parent]]["parent_values"] = []
           @groups[log[parent]]["parent_values"] << log[parent]
+          parents_list << log[parent]
         end
         # @parent_keys used to store keys (columns) for Parent Table
         # @child_keys used to store keys (columns) for Child Table
@@ -50,6 +52,7 @@ module Api
 
       if measures.present? && !measures.empty?
         measures.each do |measure_name, measure_info|
+          measure_logs = logs
           @parent_keys << measure_name
           # Dummy measure, just used for testing
           if measure_info.keys[0] == "CountOfEvents"
@@ -58,17 +61,32 @@ module Api
             end
           # Aggregation measure : Filters and then does count(*)
           elsif measure_info.keys[0] == "Count"
-            logs = logs.filter_having_keys(measure_info["Count"]["filter_having_keys"]) if measure_info["Count"]["filter_having_keys"].present?
-            logs = logs.filter(measure_info["Count"]["filter"]) if measure_info["Count"]["filter"].present?
-            logs.select("#{parent}, count(*) as #{measure_name}").group(parent).order(parent).each do |values|
-              @groups[values[parent]]["parent_values"] << values[measure_name]
+            measure_logs = measure_logs.filter_having_keys(measure_info["Count"]["filter_having_keys"]) if measure_info["Count"]["filter_having_keys"].present?
+            measure_logs = measure_logs.filter(measure_info["Count"]["filter"]) if measure_info["Count"]["filter"].present?
+            measures_data = measure_logs.select("#{parent}, count(*) as #{measure_name}").group(parent).order(parent)
+            if measures_data.blank?
+              @groups.each do |parent_name, value|
+                value["parent_values"] <<  "0"
+              end
+            else
+              done_parents = []
+              measures_data.each do |values|
+                done_parents << values[parent]
+                @groups[values[parent]]["parent_values"] << values[measure_name]
+              end
+              left_parents = parents_list - done_parents
+              left_parents.each do |left_parent|
+                @groups[left_parent]["parent_values"] <<  "0"
+              end
+                # @groups[values[parent]]["parent_values"] << "values[measure_name]"
+                # values[measure_name].present? ? @groups[values[parent]]["parent_values"] << "values[measure_name]" : @groups[values[parent]]["parent_values"] << '0'
             end
           # Measure to add key's value to parent table
           elsif measure_info.keys[0] == "AddValue"
-            logs = logs.filter_having_keys(measure_info["AddValue"]["filter_having_keys"]) if measure_info["AddValue"]["filter_having_keys"].present?
-            logs = logs.filter(measure_info["AddValue"]["filter"]) if measure_info["AddValue"]["filter"].present?
+            measure_logs = measure_logs.filter_having_keys(measure_info["AddValue"]["filter_having_keys"]) if measure_info["AddValue"]["filter_having_keys"].present?
+            measure_logs = measure_logs.filter(measure_info["AddValue"]["filter"]) if measure_info["AddValue"]["filter"].present?
             @groups.each do |parent_name, value|
-              log = logs.where("#{parent} = :parent_name", parent_name: parent_name).order(:id).first
+              log = measure_logs.where("#{parent} = :parent_name", parent_name: parent_name).order(:id).first
               log != nil ? value["parent_values"] <<  log.value(measure_info["AddValue"]["key"]) : value["parent_values"] << ""
             end
           end
