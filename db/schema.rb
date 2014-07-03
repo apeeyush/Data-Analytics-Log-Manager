5dce284a37bb8c21c6c08db26de9204450c35a0d
peeyush <peeyushagarwal1994@gmail.com>
2014-07-03 14:24:03 +0530

    Add users model for authentication
    Add sign-up, singn-in etc. using devise
    TODO: Add authentication restriction on querying

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621125340) do
+ActiveRecord::Schema.define(version: 20140703081856) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,5 +44,23 @@ ActiveRecord::Schema.define(version: 20140621125340) do
   add_index "logs", ["session"], name: "index_logs_on_session", using: :btree
   add_index "logs", ["time"], name: "index_logs_on_time", using: :btree
   add_index "logs", ["username"], name: "index_logs_on_username", using: :btree
+
+  create_table "users", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
