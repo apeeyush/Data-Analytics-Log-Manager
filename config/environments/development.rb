da181218cf72f3301a582accb04e4a64c368840e
peeyush <peeyushagarwal1994@gmail.com>
2014-07-07 21:57:33 +0530

    Configure Rails Mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,18 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  # Mailer configuration for sending mails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => ENV["EMAIL"],
+    :password             => ENV["PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,6 +65,18 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
 
+  # Mailer configuration for sending mails
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => ENV["EMAIL"],
+    :password             => ENV["PASSWORD"],
+    :authentication       => 'plain',
+    :enable_starttls_auto => true
+  }
+
   # Default URL options for Action Mailer
   config.action_mailer.default_url_options = { host: 'http://log-manager.herokuapp.com/' }
 
