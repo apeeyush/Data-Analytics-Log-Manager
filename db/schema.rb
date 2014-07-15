e5e754df275777080dc856a58174db3b9a76caea
peeyush <peeyushagarwal1994@gmail.com>
2014-07-15 16:39:24 +0530

    Add confirmable to User
    A confirmation link is sent to user on sign-up. Only after the link is opened, user is allowed to sign-in. This helps in verifying the validity and ownership of mail-id.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140703081856) do
+ActiveRecord::Schema.define(version: 20140715105340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,8 +58,12 @@ ActiveRecord::Schema.define(version: 20140703081856) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
