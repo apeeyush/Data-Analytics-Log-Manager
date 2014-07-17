30966f36e3416e587f258bcbfcf3ecf8a117b9ce
peeyush <peeyushagarwal1994@gmail.com>
2014-07-17 17:11:33 +0530

    Add devise admin for administration

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140715105340) do
+ActiveRecord::Schema.define(version: 20140717073258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "hstore"
+
+  create_table "admins", force: true do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "admins", ["email"], name: "index_admins_on_email", unique: true, using: :btree
+  add_index "admins", ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
 
   create_table "documents", force: true do |t|
     t.string   "name"
---
+++ spec/models/admin_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+RSpec.describe Admin, :type => :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
