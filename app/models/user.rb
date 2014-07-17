97218216e137409d4751846cd631c0c0c8261dfb
peeyush <peeyushagarwal1994@gmail.com>
2014-07-18 03:44:00 +0530

    Add confirmed to user
    Add confirmed:boolean to user table with default value false. User is allowed to sign in only after marked true (only accessible to admins).

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
+
+  def active_for_authentication?
+    super && approved?
+  end
+
+  def inactive_message
+    if !approved?
+      :not_approved
+    else
+      super # Use whatever other message
+    end
+  end
+
 end
