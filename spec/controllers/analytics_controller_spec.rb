e723c48451b2a6bafc4d7ed2f23659f2ddd275a9
peeyush <peeyushagarwal1994@gmail.com>
2014-07-18 17:49:55 +0530

    Update tests
    Remove /analytics/all test as the corresponding action and view has been removed.

--- spec/controllers/analytics_controller_spec.rb
+++ spec/controllers/analytics_controller_spec.rb
@@ -10,14 +10,6 @@ describe AnalyticsController, :type => :controller do
     end
   end
 
-  describe "GET 'all'" do
-    it "redirects without sign_in" do
-      get 'all'
-      expect(response.code).to eq("302")
-      expect(response).to redirect_to('/users/sign_in')
-    end
-  end
-
   describe "GET 'filter'" do
     it "redirects without sign_in" do
       get 'filter'
