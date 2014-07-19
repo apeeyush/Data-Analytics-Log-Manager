2ccba73e03ce78d107df9d951502afa3063d4da0
peeyush <peeyushagarwal1994@gmail.com>
2014-07-19 14:33:40 +0530

    Update tests

--- spec/routing/logs_routing_spec.rb
+++ spec/routing/logs_routing_spec.rb
@@ -7,32 +7,5 @@ describe LogsController, :type => :routing do
       expect(get("/logs")).to route_to("logs#index")
     end
 
-    it "routes to #new" do
-      expect(get("/logs/new")).to route_to("logs#new")
-    end
-
-    it "routes to #show" do
-      expect(get("/logs/1")).to route_to("logs#show", :id => "1")
-    end
-
-    # Edit not allowed
-    # it "routes to #edit" do
-    #   expect(get("/logs/1/edit")).to route_to("logs#edit", :id => "1")
-    # end
-
-    it "routes to #create" do
-      expect(post("/logs")).to route_to("logs#create")
-    end
-
-    # Update not allowed
-    # it "routes to #update" do
-    #   expect(put("/logs/1")).to route_to("logs#update", :id => "1")
-    # end
-
-    # Destroy not allowed
-    # it "routes to #destroy" do
-    #   expect(delete("/logs/1")).to route_to("logs#destroy", :id => "1")
-    # end
-
   end
 end
