bf8ab432971fff63b4806849b91eed7895cf638d
peeyush <peeyushagarwal1994@gmail.com>
2014-08-03 01:14:01 +0530

    Add barebone knockout to data_interactive

--- app/assets/javascripts/data_interactive.js
+++ app/assets/javascripts/data_interactive.js
@@ -35,3 +35,45 @@ $(function() {
   });
 
 });
+
+$(document).ready(function() {
+var string_filter_instance = function() {
+    var self = this;
+    self.key = ko.observable('key1');
+    self.list = ko.observable('value1');
+    self.remove = ko.observable(false);
+    self.filter_type = 'string';
+}
+
+var time_filter_instance = function() {
+    var self = this;
+    self.key = ko.observable('key1');
+    self.start_time = ko.observable();
+    self.end_time = ko.observable();
+    self.filter_type = 'time';
+}
+
+function QueryViewModel() {
+    //Data
+    var self = this;
+    this.filter = ko.observableArray([new string_filter_instance]);
+    this.filter_having_keys = ko.observable({key: ko.observable('keyyy')});
+    this.group = ko.observable('sdfgvhbn');
+
+    this.availableGroups = ko.observableArray(['username','session','event','application','activity']);
+
+    self.removeFilterInstance = function(filter_instance) {
+        self.filter.remove(filter_instance);
+    }
+
+    self.addStringFilterInstanceToFilter = function() {
+        self.filter.push(new string_filter_instance);
+    }
+
+    self.addTimeFilterInstanceToFilter = function() {
+        self.filter.push(new time_filter_instance);
+    }
+}
+
+ko.applyBindings(new QueryViewModel());
+});
