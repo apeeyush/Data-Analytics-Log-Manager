00d4239174c0933a58f2cd9c035bb2b86cdfadd4
sfentress <sfentress@concord.org>
2014-08-21 20:51:37 -0400

    Find all keys from get_explore_data; use it for group pulldown
    Here we create two arrays, one of our "recommended" list of group keys,
    and one that we get from the get_explore_data API that returns all
    the keys we have access to.
    
    Using a checkbox, the user can have the pulldown show either the recommended
    list or the entire list.

--- app/assets/javascripts/knockout_binding.js
+++ app/assets/javascripts/knockout_binding.js
@@ -70,7 +70,28 @@ function QueryViewModel() {
       add_child_data: ko.observable(false)
     });
 
-    this.recommendedGroups = ko.observableArray(['','username','session','event','application','activity']);
+    // this is async, but we don't need it immediately
+    availableKeys = [];
+    $.ajax({
+      type: "POST",
+      url: "/pages/get_explore_data",
+      data: {explore: {application: "", activity: ""}},
+      success: function(data) {
+        availableKeys = data.keys;
+      }
+    });
+
+    // allow group pulldown to use either recommended groups or all available keys
+    recommendedGroups = ['username','session','event','application','activity'];
+    this.groupList = ko.observableArray(recommendedGroups);
+    this.showRecommendedGroups = ko.observable(true);
+    this.showRecommendedGroups.subscribe(function(val) {
+      if (val) {
+        this.groupList(recommendedGroups);
+      } else {
+        this.groupList(availableKeys);
+      }
+    }, this);
 
     // Filter Operations
     self.addStringFilterInstanceToFilter = function() {
@@ -127,7 +148,6 @@ function QueryViewModel() {
     self.addTimeFilterInstanceToChildQuery = function() {
       self.child_query().filter.push(new time_filter_instance);
     };
-
 }
 
 ko.applyBindings(new QueryViewModel());
