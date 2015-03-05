025736641c00f69bf3d33be28f1028cd4c52f4a1
Richard Klancer <rpk@pobox.com>
2015-03-05 16:38:20 -0500

    Explicitly allow log mgr to be iframed in CODAP
    Rails 4 sets the X-Frame-Options header to “SAMEORIGIN”, which previously did not matter because we used to overwrite all the default headers in config/application.rb (in order to set custom Access-Control headers).
    
    When I added the rack-cors gem, I got rid of the header override. But this meant that the default X-Frame-Options header was served, and browsers running CODAP would refuse to load the log manager.

--- config/application.rb
+++ config/application.rb
@@ -38,5 +38,7 @@ module LogManager
       end
     end
 
+    # We actually expect the Log Manager to be loaded into an iframe
+    config.action_dispatch.default_headers.delete 'X-Frame-Options'
   end
 end
