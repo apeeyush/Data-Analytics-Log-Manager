53508dc902eb8935808b2224380742443862216d
Piotr Janik <janikpiotrek@gmail.com>
2015-11-25 15:54:30 +0100

    Do not load spreadsheet file when client is checking status only
    [#108237744]

--- app/controllers/log_spreadsheets_controller.rb
+++ app/controllers/log_spreadsheets_controller.rb
@@ -5,7 +5,9 @@ class LogSpreadsheetsController < ApplicationController
   before_action :authenticate_user!
 
   def status
-    spreadsheet = LogSpreadsheet.find(params.require(:id))
+    # WARNING: note that 'file' field can be huge sometimes. Don't load it when it's not needed,
+    #          limit fields to 'status' and 'status_msg' only.
+    spreadsheet = LogSpreadsheet.select('status', 'status_msg').find(params.require(:id))
     render json: {status: spreadsheet.status, status_msg: spreadsheet.status_msg}
   end
 
@@ -16,6 +18,6 @@ class LogSpreadsheetsController < ApplicationController
       render nothing: true, status: 400
       return
     end
-    send_data spreadsheet.file.force_encoding('binary'), filename: "logs-#{spreadsheet.id}.csv", type: 'text/csv'
+    send_data spreadsheet.file, filename: "logs-#{spreadsheet.id}.csv", type: 'text/csv'
   end
 end
