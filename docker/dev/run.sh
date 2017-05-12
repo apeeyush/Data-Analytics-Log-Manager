77b7b03c0a598fca135d513d5353b63042f3d653
Jason Simpson <jsimpson@concord.org>
2017-05-12 10:58:31 -0700

    Fix docker startup script.
    [#145303211]

--- docker/dev/run.sh
+++ docker/dev/run.sh
@@ -3,7 +3,8 @@
 # Run this app in a docker container
 #
 
-DB_INIT_FILE=/config/db.initialized
+DB_INIT_FILE=config/db.initialized
+PID_FILE=tmp/pids/server.pid
 
 #
 # Install gems
@@ -18,6 +19,10 @@ npm install
 if [ ! -f $DB_INIT_FILE ]; then
     bundle exec rake db:migrate
     touch $DB_INIT_FILE
+fi
+
+if [ -f $PID_FILE ]; then
+    rm -rf $PID_FILE
 fi
 
 bundle exec rails s -b 0.0.0.0
