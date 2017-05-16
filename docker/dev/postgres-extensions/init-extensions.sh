2ddf8fe9dbc6cf6031a44584480a0a224a066183
Jason Simpson <jsimpson@concord.org>
2017-05-15 20:28:58 -0700

    Add postgres extensions to docker image config.
    [#145303211]

--- docker/dev/postgres-extensions/init-extensions.sh
+++ docker/dev/postgres-extensions/init-extensions.sh
@@ -2,11 +2,19 @@
 #
 # Configure postgres docker image to use the extensions we need
 #
+
+#
+# Run "set +e" because these init scrupts seem to run "set -e" by default,
+# which doesn't allow us to check exit failures from grep or otherwise.
+#
+set +e
+
 PG_CONF=/var/lib/postgresql/data/postgresql.conf
 
 grep pg_stat_statements $PG_CONF
 
 if [ $? != 0 ]; then
+    echo "Adding pg_stat_statements to config ..."
     echo "shared_preload_libraries = 'pg_stat_statements'" >> $PG_CONF
 fi
 
